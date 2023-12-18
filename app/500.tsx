9f9eae9eaec49d09a99787c36695a6f2069a6966
lohith warlock <lohxt.space@gmail.com>
2023-12-18 15:30:52 +0530

    feat: some default routes specific to cf deploy

---
+++ app/500.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export const runtime = "edge";
+
+export default function Custom500() {
+  return <h1>404 - Page Not Found</h1>;
+}
---
+++ app/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const runtime = "edge";
+
+export default function Error() {
+  return (
+    <div>
+      <h2>Error</h2>
+      <p>Could not find requested resource</p>
+    </div>
+  );
+}
